1579ec7a374045624d0522528a7c7a35a12a25ff
Wenming Fu <winminy@163.com>
2021-09-27 14:26:45 +0800

    update action for building image

--- generated/teamcity-server-local-linux-arm64-20.04.sh
+++ generated/teamcity-server-local-linux-arm64-20.04.sh
@@ -3,4 +3,4 @@ cd ..
 docker pull ubuntu:20.04
 echo TeamCity/buildAgent > context/.dockerignore
 echo TeamCity/temp >> context/.dockerignore
-docker build -f "generated/linux/Server/UbuntuARM/20.04/Dockerfile" -t teamcity-server:local-linux-arm64-20.04 "context"
+docker build -f "generated/linux/Server/UbuntuARM/20.04/Dockerfile" -t wayne5aw/teamcity-server:arm64-20.04 "context"
